420f043ed6c10a2b5e60e6d032efd36e17fc7407
Kiran ojha <kiranojhanp>
2020-12-07 19:33:09 +0545

    HomeScreen is added | display products from js object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { Container } from "react-bootstrap";
+
 // components and screens
 import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={RegisterScreen} exact />
+      <Container>
+        <Route path="/register" component={RegisterScreen} exact />
+        <Route path="/" component={HomeScreen} exact />
+      </Container>
     </Router>
   );
 }
