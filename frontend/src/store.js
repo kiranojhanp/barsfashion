1382ce216e7b186c1f6e962f168fd2aca66559f9
Kiran <kiranojha1226@gmail.com>
2020-12-10 14:57:37 +0545

    Register frontend successfully implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,18 +4,13 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 // reducers
 import { userRegisterReducer } from "./reducers/userReducer";
-
+ 
 // combining all the reducers
 const reducer = combineReducers({ userRegister: userRegisterReducer });
 
 const middleware = [thunk];
 
-// some values from the localstorage
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
-
-const initialState = { userLogin: { userInfo: userInfoFromStorage } };
+const initialState = {};
 
 const store = createStore(
   reducer,
