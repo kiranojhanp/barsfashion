ebf2c6a577e3b94cb90dbfda028ece738b731682
rochan <roch007maharjan@gmail.com>
2020-12-13 11:46:15 +0545

    login controller update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,29 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 
+
+// @desc  Authenticate the user & get token
+// @route  POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
 // @desc  Register a new user
 // @route  POST /api/users/register
 // @access Public
